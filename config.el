e2b923acfaed993d1c6ff99caaf232011e7b3078
Prem Modha <premmodhaofficial@gmail.com>
2023-07-03 23:36:27 +0530

    back to clangd i guess....

--- config.el
+++ config.el
@@ -99,16 +99,16 @@
 (setq scroll-margin 5)
 
 ;; lsp for the cc
-;; (setq lsp-clients-clangd-args '("-j=3"
-;;                                 "--background-index"
-;;                                 "--clang-tidy"
-;;                                 "--completion-style=detailed"
-;;                                 "--header-insertion=never"
-;;                                 "--header-insertion-decorators=0"))
-;; (after! lsp-clangd (set-lsp-priority! 'clangd 2))
+(setq lsp-clients-clangd-args '("-j=3"
+                                "--background-index"
+                                "--clang-tidy"
+                                "--completion-style=detailed"
+                                "--header-insertion=never"
+                                "--header-insertion-decorators=0"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
 
 
 ;; for ccls
-(after! ccls
-  (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
-  (set-lsp-priority! 'ccls 2)) ; optional as ccls is the default in Doom
+;; (after! ccls
+;;   (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
+;;   (set-lsp-priority! 'ccls 2)) ; optional as ccls is the default in Doom
