20f513e2dbe65ffef422c3910388ee3b6148022d
Prem Modha <premmodhaofficial@gmail.com>
2023-07-15 13:32:53 +0530

    Spacemacs theme set and font settings

--- config.el
+++ config.el
@@ -31,8 +31,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 22 :weight 'light))
+(setq doom-theme 'spacemacs-dark)
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 18 :weight 'light))
 
 ;; full name: JetBrainsMono Nerd Font:pixelsize=22:foundry=JB:weight=extra-bold:slant=italic:width=normal:spacing=100:scalable=true
 ;; This determines the style of line numbers in effect. If set to `nil', line
